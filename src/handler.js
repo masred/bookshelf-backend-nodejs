8e6aa531c8e7d312ef0044a26177e06748884ffc
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-11-24 00:02:11 +0700

    feat: add create, get all and get by id book data

---
+++ src/handler.js
@@ -0,0 +1,115 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBook = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+  const finished = false;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = new Date().toISOString();
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  if (name == null) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message:
+        'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+
+    response.code(400);
+    return response;
+  }
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  });
+
+  response.code(500);
+  return response;
+};
+
+const getAllBooks = () => ({
+  status: 'success',
+  data: {
+    books: books.map((show) => {
+      const { id, name, publisher } = show;
+      return { id, name, publisher };
+    }),
+  },
+});
+
+const getBookById = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((b) => b.id === bookId)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addBook, getAllBooks, getBookById };
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const { addBook, getAllBooks, getBookById } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBook,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooks,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookById,
+  },
+];
+
+module.exports = routes;
